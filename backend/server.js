9404c388487249e6376d7e70a7cc2071859805dd
mp21695 <muskaanpanda115@gmail.com>
2026-03-07 00:19:40 +0530

    Deploy backend to AWS Elastic Beanstalk

--- backend/server.js
+++ backend/server.js
@@ -199,6 +199,7 @@ app.post("/ask-ai", async (req, res) => {
     res.status(500).json({ error: "Bedrock request failed" });
   }
 });
-app.listen(5000, () => {
-  console.log("Backend running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running");
 });
